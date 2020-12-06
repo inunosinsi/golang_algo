e4901bebb8f68b00e5e048ba7579e02c76dc8598
inunosinsi <tsuyoshi@saitodev.co>
2020-12-06 15:21:21 +0900

    Refactoring

--- tree/main.go
+++ tree/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	values := []int{9, 4, 15, 2, 6, 12, 17, 3, 1, 7, 5, 8, 11, 13, 14, 16}
+	values := []int{9, 4, 15, 2, 6, 12, 15, 17, 3, 1, 7, 5, 8, 11, 13, 14, 16}
 	bt := tree.New(values[0])
 	for idx, i := range values {
 		if idx == 0 {
