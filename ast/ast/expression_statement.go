02bb36ed9010e817d66a950059fe25fa3e131520
inunosinsi <tsuyoshi@saitodev.co>
2021-08-30 14:49:20 +0900

    [Add]Added parseExpression

---
+++ ast/ast/expression_statement.go
@@ -0,0 +1,11 @@
+package ast
+
+import "../token"
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return string(es.TokenLiteral()) }
--- ast/parser/parser.go
+++ ast/parser/parser.go
@@ -27,7 +27,18 @@ func (p *Parser) Parse() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	//処理
+	//処理 tokenを二回進めることで、curTokenに最初のトークン、peekTokenに２つ目のトークンが格納される
+	p.nextToken()
+	p.nextToken()
+
+	//再帰下降構文解析
+	for p.curToken.TokenType != token.EOF {
+		// stmt := p.parseStatement()
+		// if stmt != nil {
+		// 	program.Statements = append(program.Statements, stmt)
+		// }
+		p.nextToken()
+	}
 
 	return program
 }
@@ -37,10 +48,21 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
-// func (p *Parser) parseProgram() *ast.Program {
-//
-// }
-//
-// func (p *Parser) parseExpression() ast.Expression {
-//
-// }
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.TokenType {
+	default:
+		return p.parseExpressionStatement()
+	}
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	// stmt.Expression = p.parseExpression(LOWEST)
+	//
+	// if p.peekTokenIs(token.SEMICOLON) {
+	// 	p.nextToken()
+	// }
+
+	return stmt
+}
