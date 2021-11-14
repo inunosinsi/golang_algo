74915f930bd23248a6eceba34d9a032e4e0be469
inunosinsi <tsuyoshi@saitodev.co>
2021-11-15 06:28:02 +0900

    [Add]Added hashtable's practice code.

--- hashtable/hash/hash.go
+++ hashtable/hash/hash.go
@@ -1,1 +1,15 @@
 package hash
+
+import (
+	"crypto/md5"
+	"strconv"
+)
+
+//変数名からハッシュ値を作成する。コードの都合上、ハッシュ値は0〜9までの整数型にする
+func MakeHashValue(ident string) int {
+	hash := md5.Sum([]byte(ident))
+	str := strconv.Itoa(int(hash[0]))
+	first := str[0:1]
+	h, _ := strconv.Atoi(first)
+	return h
+}
--- hashtable/main.go
+++ hashtable/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"./hash"
+	"./node"
+)
+
+func main() {
+	table := make([]*node.Node, 10, 10)
+	n := node.New("hensu1", "cat")
+	h := hash.MakeHashValue(n.Ident)
+	table[h] = n
+
+	n = node.New("hensu2", "dog")
+	h = hash.MakeHashValue(n.Ident)
+	table[h] = n
+
+	result := search(table, "hensu2")
+	fmt.Println(result)
+
+	// n = node.New("hensu3", "rabbit")
+	// h = hash.MakeHashValue(n.Ident)
+}
+
+// 変数名で変数の値を検索する。変数テーブルに指定の変数名がなければ、文字列でnilの値を返す
+func search(table []*node.Node, ident string) string {
+	//ハッシュ値を用いて検索を高速化する
+	h := hash.MakeHashValue(ident)
+	n := table[h]
+
+	// 念の為に変数名が正しいか？を確認
+	if n.Ident == ident {
+		return n.Value
+	}
+
+	return "nil"
 }
