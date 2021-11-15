3575893670e84490bff42d4d8ff187cf6cf398b5
inunosinsi <tsuyoshi@saitodev.co>
2021-11-15 14:16:10 +0900

    [Add]Added var ast.

---
+++ compile/ast/var.go
@@ -0,0 +1,32 @@
+package ast
+
+import (
+	"bytes"
+
+	"../token"
+)
+
+type VarStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (va *VarStatement) statementNode()       {}
+func (va *VarStatement) TokenLiteral() string { return string(va.Token.Literal) }
+
+func (va *VarStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(va.TokenLiteral() + " ")
+	out.WriteString(va.Name.String())
+	out.WriteString(" = ")
+
+	if va.Value != nil {
+		out.WriteString(va.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
