a0e8d5a07a79d160fb612774238bead45263477e
inunosinsi <tsuyoshi@saitodev.co>
2022-03-20 06:48:00 +0900

    [Add]Added block stmt parser fn.

---
+++ vm/parser/block.go
@@ -0,0 +1,24 @@
+package parser
+
+import (
+	"../ast"
+	"../token"
+)
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	// }が現れるまでトークンを読み進める 改行があれば次の文を読み始める
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return block
+}
