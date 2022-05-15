418b505fb7d47784a04af0ee18e5765e9893cb9b
inunosinsi <tsuyoshi.s.saito@gmail.com>
2022-05-16 06:30:42 +0900

    [Add]Added array's code on main.go

--- vm/main.go
+++ vm/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 
-	"./code"
-	"./compiler"
 	"./lexer"
 	"./parser"
 )
@@ -17,14 +15,19 @@ func main() {
 
 	// var res = fn(5, 3);
 	// echo res;`
-	input := `function hikizan(a, b){
-	var c = a + b;
-	return c;
-}
+	// 	input := `function hikizan(a, b){
+	// 	var c = a + b;
+	// 	return c;
+	// }
 
-var kotae = hikizan(5, 3);
-echo kotae;
-`
+	// var kotae = hikizan(5, 3);
+	// echo kotae;
+	// `
+
+	//input := `var a = 1;`
+	input := `var arr = [1 ,2, 3];
+echo arr[1];`
+
 	l := lexer.New(input)
 	p := parser.New(l) //lexerをparserの中に組み込む
 	program := p.Parse()
@@ -32,19 +35,19 @@ echo kotae;
 	//抽象構文木が正しくできれば出力される
 	fmt.Println(program.String())
 
-	c := compiler.New()
-	_ = c.Compile(program) //エラーを拾う事を無しにする
+	// c := compiler.New()
+	// _ = c.Compile(program) //エラーを拾う事を無しにする
 
-	// 中間コードを出力する
-	if len(c.Opcodes) > 0 {
-		for _, opcode := range c.Opcodes {
-			if opcode.Mnemonic == code.LABEL || opcode.Mnemonic == code.FUNC {
-				fmt.Printf("%s\n", string(opcode.Operand))
-			} else {
-				fmt.Printf("%s %s\n", code.GetCode(opcode.Mnemonic), string(opcode.Operand))
-			}
-		}
-	}
+	// // 中間コードを出力する
+	// if len(c.Opcodes) > 0 {
+	// 	for _, opcode := range c.Opcodes {
+	// 		if opcode.Mnemonic == code.LABEL || opcode.Mnemonic == code.FUNC {
+	// 			fmt.Printf("%s\n", string(opcode.Operand))
+	// 		} else {
+	// 			fmt.Printf("%s %s\n", code.GetCode(opcode.Mnemonic), string(opcode.Operand))
+	// 		}
+	// 	}
+	// }
 
 	// result := vm.Eval(c.Opcodes)
 	// fmt.Println(result)
