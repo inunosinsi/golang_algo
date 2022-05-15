3e82bcd6d3e530ef2a893fa2c84d7890b1f68176
inunosinsi <tsuyoshi.s.saito@gmail.com>
2022-05-16 06:17:52 +0900

    [Change]Divided call file into call file and list file.

---
+++ vm/parser/list.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"../ast"
+	"../token"
+)
+
+func (p *Parser) parseExpressionList(end int) []ast.Expression {
+	list := []ast.Expression{}
+
+	if p.peekTokenIs(end) {
+		p.nextToken()
+		return list
+	}
+
+	p.nextToken()
+	list = append(list, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		list = append(list, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(end) {
+		return nil
+	}
+
+	return list
+}
